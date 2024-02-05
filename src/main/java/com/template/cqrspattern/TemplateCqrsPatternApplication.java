f358084fe80ef190b77b7b66b118012718a5389c
piopoi <kangsw1988@gmail.com>
2024-02-05 22:43:20 +0900

    Initial Commit

---
+++ src/main/java/com/template/cqrspattern/TemplateCqrsPatternApplication.java
@@ -0,0 +1,13 @@
+package com.template.cqrspattern;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TemplateCqrsPatternApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(TemplateCqrsPatternApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/template/cqrspattern/TemplateCqrsPatternApplicationTests.java
@@ -0,0 +1,13 @@
+package com.template.cqrspattern;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class TemplateCqrsPatternApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
